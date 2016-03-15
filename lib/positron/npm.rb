eeff7ba73952db644f003dc52168b15ddac75de4
Brandon Mathis <brandon@imathis.com>
2016-03-15 17:56:03 -0500

    Mocked out NPM commands for creating package.json if it does not exist

--- lib/positron/npm.rb
+++ lib/positron/npm.rb
@@ -16,18 +16,48 @@ module Positron
     end
 
     def setup
-      system "npm install #{node_dependencies} -D"
+      require 'json'
+
+      if File.exist?(package_path)
+        install
+      else
+        if bool_ask("No package.json found at #{config[:cli_path]}\nWould you like to create one?")
+          init_package_json
+          install
+        else
+          return
+        end
+      end
+    end
+
+    def init_package_json
+      # TODO: Create a package.json file with dev depenencies
+    end
+
+    def install
+      Dir.chdir(config[:cli_path]) do
+        system "npm install #{node_dependencies} -D"
+      end
+    end
+
+    def bool_ask(question)
+      puts "#{question} (Y\\n): "
+      response = $stdin.gets.chomp
+      response.nil? || !response.downcase.start_with?('n')
+    end
+
+    def package_path
+      File.expand_path(File.join(config[:cli_path], 'package.json'))
     end
 
     def node_dependencies
-      NODE_DEPENDENCIES.map do |k,v| 
+      DEPENDENCIES.map do |k,v| 
         "#{k}@\"#{v}\""
       end.join(' ')
     end
 
     def node_dependencies_json
-      require 'json'
-      JSON.generate(NODE_DEPENDENCIES)
+      JSON.generate(DEPENDENCIES)
     end
 
   end
