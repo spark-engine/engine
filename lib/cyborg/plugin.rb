c62893d6c44f9831bac94a1ca4b4ac873fdb7d97
Brandon Mathis <brandon@imathis.com>
2016-09-19 09:54:36 -0500

    Fixed config instance variable

--- lib/cyborg/plugin.rb
+++ lib/cyborg/plugin.rb
@@ -89,7 +89,7 @@ module Cyborg
         root:          @gem.full_gem_path,
         version:       @gem.version.to_s,
         gem_name:      @gem.name,
-        gzip_in_production?: true,
+        gzip_in_production: true,
         paths: {
           stylesheets: "app/assets/stylesheets/#{name}",
           javascripts: "app/assets/javascripts/#{name}",
@@ -100,6 +100,10 @@ module Cyborg
       options.each do |k,v|
         set_instance(k.to_s,v) 
       end
+    end
+
+    def gzip_in_production?
+      @gzip_in_production
     end
 
     def expand_asset_paths
