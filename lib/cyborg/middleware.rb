26b2edea5e56250e3a23e05f612ad10925e93922
Brandon Mathis <brandon@imathis.com>
2017-01-26 11:27:51 -0600

    Support Rails 4

--- lib/cyborg/middleware.rb
+++ lib/cyborg/middleware.rb
@@ -15,31 +15,67 @@ module Cyborg
       end
     end
   end
+
   class StaticAssets
-    def initialize(app, path, index: 'index', headers: {}, engine_name: nil)
-      @app = app
-      @engine_name = engine_name
-      @file_handler = ActionDispatch::FileHandler.new(path, index: index, headers: headers)
-    end
 
-    def call(env)
-      req = Rack::Request.new env
-      prefix = File.join Application.config.assets.prefix, @engine_name
+    # Rails 5 middleware patch
+    if Gem::Version.new(Rails.version) >= Gem::Version.new('5')
 
-      if req.get? || req.head?
-        path = req.path_info.chomp('/'.freeze)
-
-        if path.start_with? prefix
-          path = path.remove /\A#{prefix}\//
-
-          if match = @file_handler.match?(path)
-            req.path_info = match
-            return @file_handler.serve(req)
-          end
-        end
+      def initialize(app, path, index: 'index', headers: {}, engine_name: nil)
+        @app = app
+        @engine_name = engine_name
+        @file_handler = ActionDispatch::FileHandler.new(path, index: index, headers: headers)
       end
 
-      @app.call(req.env)
+      def call(env)
+        req = Rack::Request.new env
+        prefix = File.join Application.config.assets.prefix, @engine_name
+
+        if req.get? || req.head?
+          path = req.path_info.chomp('/'.freeze)
+
+          if path.start_with? prefix
+            path = path.remove /\A#{prefix}\//
+
+            if match = @file_handler.match?(path)
+              req.path_info = match
+              return @file_handler.serve(req)
+            end
+          end
+        end
+
+        @app.call(req.env)
+      end
+
+    # Rails 4 middleware patch
+    else
+
+      def initialize(app, path, cache_control=nil, engine_name: nil)
+        @app = app
+        @engine_name = engine_name
+        @file_handler = ::ActionDispatch::FileHandler.new(path, cache_control)
+      end
+
+      def call(env)
+        prefix = File.join Application.config.assets.prefix, @engine_name
+
+        case env['REQUEST_METHOD']
+        when 'GET', 'HEAD'
+          path = env['PATH_INFO'].chomp('/')
+
+          if path.start_with? prefix
+            path = path.remove /\A#{prefix}\//
+
+            if match = @file_handler.match?(path)
+              env["PATH_INFO"] = match
+              return @file_handler.call(env)
+            end
+          end
+        end
+
+        @app.call(env)
+      end
     end
   end
+
 end
--- lib/cyborg/version.rb
+++ lib/cyborg/version.rb
@@ -1,3 +1,3 @@
 module Cyborg
-  VERSION = "0.5.0"
+  VERSION = "0.5.1"
 end
